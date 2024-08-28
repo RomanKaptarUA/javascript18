38468da032e64153751b3fb156deb78e5c850c99
RomanKaptarUA <romankaptar82@gmail.com>
2024-08-28 16:31:06 +0300

    .

---
+++ script.js
@@ -0,0 +1,27 @@
+( ( )  =>  { 
+    const  mobileMenu  =  документ . querySelector ( '.js-menu-container' ) ; 
+    const  openMenuBtn  =  документ . querySelector ( '.js-open-menu' ) ; 
+    const  closeMenuBtn  =  документ . querySelector ( '.js-close-menu' ) ; 
+
+    const  toggleMenu  =  ( )  =>  { 
+      const  isMenuOpen  = 
+        openMenuBtn . getAttribute ( 'aria-expanded' )  ===  'true'  ||  false ; 
+      openMenuBtn . setAttribute ( 'aria-expanded' ,  ! isMenuOpen ) ; 
+      mobileMenu . classList . перемикати ( 'відкрито' ) ; 
+
+      const  scrollLockMethod  =  ! isMenuOpen 
+        ? 'disableBodyScroll' 
+        : 'enableBodyScroll' ; 
+      bodyScrollLockscrollLockMethod ; 
+    } ; 
+
+    openMenuBtn . addEventListener ( 'клацання' ,  toggleMenu ) ; 
+    closeMenuBtn . addEventListener ( 'клацання' ,  toggleMenu ) ; 
+
+    вікно . matchMedia ( '(мін. ширина: 768 пікселів)' ) . addEventListener ( 'змінити' ,  e  =>  { 
+      if  ( ! e . matches )  return ; 
+      mobileMenu . classList . видалити ( 'is-open' ) ; 
+      openMenuBtn . setAttribute ( 'aria-expanded' ,  false ) ; 
+      bodyScrollLock . enableBodyScroll ( document.body ) ;​​ 
+    } ) ; 
+  } ) ( ) ;
