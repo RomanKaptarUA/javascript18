43860b61059c92398104153fecac5f80951a463f
RomanKaptarUA <romankaptar82@gmail.com>
2024-09-10 15:17:27 +0300

    ,

--- script.js
+++ script.js
@@ -45,13 +45,64 @@
 //  added.textContent = 'Приправи';
 //  console.log(added);
 
+// 3
+
+const images = [
+  {
+   url:
+    'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+   alt: 'White and Black Long Fur Cat',
+  },
+  {
+   url:
+    'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+   alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+   url:
+    'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+   alt: 'Group of Horses Running',
+  },
+ ];
+
+ const gallery = document.getElementById('gallery');
+gallery.insertAdjacentHTML("afterbegin", '<li> https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260 </li>');
 
 
 
-document.addEventListener('keydown' (event) =>{
-  const counterValue = document.getElementById('value');
-  const step = '1';
-  if (event.key === ''){
-    counterValue = `${step}`;
-  }
-})
+
+
+
+
+
+
+
+// 4
+// let counterValue = 0;
+
+
+// const valueSpan = document.getElementById('value');
+
+
+// function increment() {
+//     counterValue += 1;
+//     update();
+// }
+
+
+// function decrement() {
+//     counterValue -= 1;
+//     update();
+// }
+
+
+// function update() {
+//     valueSpan.textContent = counterValue;
+// }
+
+
+// const decrementButton = document.querySelector('button[data-action="decrement"]');
+// const incrementButton = document.querySelector('button[data-action="increment"]');
+
+// decrementButton.addEventListener('click', decrement);
+// incrementButton.addEventListener('click', increment);
